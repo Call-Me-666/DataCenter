4e718e722e1b1fb087716d94624311d6eb6271b0
zx842746244 <842746244@qq.com>
2022-07-20 13:58:52 +0800

    1.提交test文件

---
+++ center-user/src/main/java/cn/zjx/user/controller/TestController.java
@@ -0,0 +1,11 @@
+package cn.zjx.user.controller;
+
+/**
+ * @Description:
+ * @Program: data-center
+ * @Author: zjx
+ * @Create: 2022-07-20 13:58:29
+ * @Version: 1.0
+ **/
+public class TestController {
+}
